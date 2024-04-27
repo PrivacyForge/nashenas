3a014e0bbc11e94d0eab485b0c61831c97cfd0b7
Pesar <pesar369@gmail.com>
2024-04-27 16:22:26 +0330

    init

---
+++ go.mod
@@ -0,0 +1,14 @@
+module main
+
+go 1.22.2
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	gorm.io/driver/sqlite v1.5.5 // indirect
+	gorm.io/gorm v1.25.9 // indirect
+)
---
+++ main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	// "encoding/json"
+	// "fmt"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"log"
+	"strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type OTP struct {
+	gorm.Model
+	Userid   int64  `gorm:"size: 255"`
+	Username string `gorm:"size: 255"`
+	Code     string `gorm:"size: 255"`
+}
+
+type User struct {
+	gorm.Model
+	ID        int64  `gorm:"primaryKey"`
+	Userid    int64  `gorm:"size: 255"`
+	Username  string `gorm:"size: 255"`
+	PublicKey string `gorm:"size: 255"`
+}
+
+type SetKeyBody struct {
+	PublicKey string `json:"public_key"`
+}
+
+func main() {
+	godotenv.Load(".dev.env")
+	PORT := os.Getenv("PORT")
+	DB_PATH := os.Getenv("DB_PATH")
+	BOT_TOKEN := os.Getenv("BOT_TOKEN")
+
+	bot, err := tgbotapi.NewBotAPI(BOT_TOKEN)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open(DB_PATH), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	db.AutoMigrate(&OTP{})
+	db.AutoMigrate(&User{})
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates := bot.GetUpdatesChan(u)
+
+	http.HandleFunc("POST /set-key", func(w http.ResponseWriter, r *http.Request) {
+		id := r.URL.Query().Get("id")
+
+		body, _ := io.ReadAll(r.Body)
+
+		var resbody SetKeyBody
+		json.Unmarshal(body, &resbody)
+
+		fmt.Println(resbody.PublicKey)
+
+		var result User
+		db.Model(&result).Where("id = ?", id).Update("public_key", resbody.PublicKey)
+
+		if result.PublicKey != "" {
+			io.WriteString(w, "Key set successfully.")
+		} else {
+			w.WriteHeader(400)
+			io.WriteString(w, "Error.")
+
+		}
+
+	})
+
+	http.HandleFunc("GET /sign-up", func(w http.ResponseWriter, r *http.Request) {
+		code := r.URL.Query().Get("code")
+
+		var result OTP
+		db.Where("code = ?", code).Find(&result)
+
+		if result.ID != 0 {
+			db.Create(&User{Userid: result.Userid, Username: result.Username, PublicKey: ""})
+			io.WriteString(w, "The user has been generated.")
+		} else {
+			w.WriteHeader(400)
+			io.WriteString(w, "Code is invalid")
+		}
+
+	})
+
+	go http.ListenAndServe(":"+PORT, nil)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+
+			switch update.Message.Command() {
+			case "start":
+				query := strings.Split(update.Message.Text, " ")
+
+				fmt.Println(query)
+				if len(query) > 1 && query[1] == "otp" {
+					code := uuid.NewString()
+					db.Create(&OTP{
+						Code:     code,
+						Userid:   update.Message.Chat.ID,
+						Username: update.Message.From.UserName,
+					})
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, code)
+					msg.ReplyToMessageID = update.Message.MessageID
+
+					bot.Send(msg)
+				} else {
+					msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hello")
+					msg.ReplyToMessageID = update.Message.MessageID
+
+					bot.Send(msg)
+				}
+
+			}
+
+		}
+	}
+}
