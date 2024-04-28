dce641f01084bc5dfb7978b554f9390eeead11c1
Pesar <pesar369@gmail.com>
2024-04-28 11:05:28 +0330

    feat: add tailwind & add some pages

---
+++ src/views/Auth.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="flex justify-center items-center h-screen w-screen">
+    <div>
+      <a
+        class="bg-[#119af5] text-white font-semibold py-6 px-12 rounded-lg text-sm"
+        href="https://telegram.me/fihawdhaw02bot?start=otp"
+        target="_blank"
+      >
+        Start Our Telegram Bot
+      </a>
+    </div>
+  </div>
+</template>
