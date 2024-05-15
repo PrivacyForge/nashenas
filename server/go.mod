3374f0a39f06f420d3e2f4d4300a62cbf6fc6ad0
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2024-05-15 23:01:59 +0330

    feat: add http proxy support

--- server/go.mod
+++ server/go.mod
@@ -25,5 +25,5 @@ require (
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
-	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
 )
--- server/telegram/bot.go
+++ server/telegram/bot.go
@@ -1,7 +1,12 @@
 package telegram
 
 import (
+	"flag"
 	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
 
 	"github.com/PrivacyForge/nashenas/configs"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -10,9 +15,31 @@ import (
 var Bot *tgbotapi.BotAPI
 
 func InitBot() tgbotapi.UpdatesChannel {
-
 	var err error
-	Bot, err = tgbotapi.NewBotAPI(configs.BotToken)
+	var httpProxyString string
+	var httpProxyUrl *url.URL
+
+	flag.StringVar(&httpProxyString, "http-proxy", "", "Set custom proxy server.")
+	flag.Parse()
+
+	if len(strings.TrimSpace(httpProxyString)) > 0 {
+		httpProxyUrl, err = url.Parse(httpProxyString)
+
+		if err != nil {
+			panic("Proxy Server Is Not Valid")
+		}
+
+		httpProxy := http.ProxyURL(httpProxyUrl)
+
+		Bot, err = tgbotapi.NewBotAPIWithClient(configs.BotToken, "https://api.telegram.org/bot%s/%s", &http.Client{
+			Transport: &http.Transport{
+				Proxy:               httpProxy,
+				TLSHandshakeTimeout: 10 * time.Second,
+			},
+		})
+	} else {
+		Bot, err = tgbotapi.NewBotAPI(configs.BotToken)
+	}
 
 	if err != nil {
 		panic("telegram bot connection failed.")
