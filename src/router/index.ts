1f00a06d93b8fdf7f9d7f754345f0eaef47361d4
Saman-Safaei-Dev <xsamansafaeix2@gmail.com>
2024-06-03 15:38:03 +0330

    refactor: change order of routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,47 +1,44 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import AuthMiddleware from '@/middlewares/auth'
+import { createRouter, createWebHistory } from "vue-router";
+import AuthMiddleware from "@/middlewares/auth";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      redirect: 'inbox',
-    },
-    {
-      path: '/auth',
-      name: 'auth',
-      component: () => import('@/views/Auth.vue'),
-    },
-    {
-      path: '/confirm/:code',
-      name: 'confirm',
-      component: () => import('@/views/Confirm.vue'),
-    },
-    {
-      path: '/setup',
-      name: 'setup',
-      component: () => import('@/views/Setup.vue'),
-      beforeEnter: AuthMiddleware,
-    },
-    {
-      path: '/@:username([a-zA-Z]{1}[a-zA-Z0-9]{4,})',
-      name: 'profile',
-      component: () => import('@/views/Profile.vue'),
-    },
-    {
-      path: '/',
-      component: () => import('@/layouts/main.vue'),
+      path: "/",
+      redirect: { name: "inbox" },
+      component: () => import("@/layouts/main.vue"),
       children: [
         {
-          path: '/inbox',
-          name: 'inbox',
-          component: () => import('@/views/Inbox.vue'),
+          path: "/inbox",
+          name: "inbox",
+          component: () => import("@/views/Inbox.vue"),
           beforeEnter: AuthMiddleware,
         },
       ],
     },
+    {
+      path: "/auth",
+      name: "auth",
+      component: () => import("@/views/Auth.vue"),
+    },
+    {
+      path: "/confirm/:code",
+      name: "confirm",
+      component: () => import("@/views/Confirm.vue"),
+    },
+    {
+      path: "/setup",
+      name: "setup",
+      component: () => import("@/views/Setup.vue"),
+      beforeEnter: AuthMiddleware,
+    },
+    {
+      path: "/@:username([a-zA-Z]{1}[a-zA-Z0-9]{4,})",
+      name: "profile",
+      component: () => import("@/views/Profile.vue"),
+    },
   ],
-})
+});
 
-export default router
+export default router;
