ec016fb3bd4a991e8ead555e56bb65ea72e03527
yasha <pesar369@gmail.com>
2024-08-22 01:45:55 +0330

    update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+function copy(value: string) {
+  navigator.clipboard.writeText(value)
+}
 </script>
 
 <template>
-  {{ $route.fullPath }}
+  <span @click="copy($route.fullPath)">click</span>
   <RouterView />
 </template>
